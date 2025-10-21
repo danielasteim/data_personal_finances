82b9c375029296947adf2849c585ba5113c42983
Daniela <danieladesa01@gmail.com>
2025-10-21 08:44:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+PERSONAL FINANCES PIPELINE
+
+This personal project aims to automate my credit card PDF montly bills into data, including classification of spending categories with machine learning, preparing the data to be used in interactive dashboards. 
+
+
+## USED TECNOLOGIES
+- **Python** (pandas, scikit-learn, pdfplumber)
+- **Power BI** / **Tableau**
+- **Git + GitHub**
+
+## HOW TO RUN
+```bash
+git clone https://github.com/SEU_USUARIO/financas-pessoais-pipeline.git
+cd pipeline-personal-finances
+python -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+python main.py ```
+
+## DASHBOARDS VISUALIZATIONS
---
+++ main.py
@@ -0,0 +1,47 @@
+import pdfplumber
+import re
+import pandas as pd
+
+FILEPATH = "data/raw/Fatura2025-10-05.pdf"
+
+def extract_transactions(filepath, pw=None, start_page=2):
+    transactions = []
+
+    with pdfplumber.open(filepath, password=pw) as pdf:
+        # Itera pelas páginas a partir da 3ª (índice 2)
+        for page_num in range(start_page, len(pdf.pages)):
+            page = pdf.pages[page_num]
+            text = page.extract_text()
+            
+            if not text:
+                continue
+            
+            # Se quiser verificar o texto, descomente:
+            # print(text)
+            
+            # Encontra apenas a parte da tabela (após "Data Descrição R$")
+            if "Data Descrição R$" in text:
+                table_text = text.split("Data Descrição R$")[-1]
+            else:
+                continue
+
+            # Remove cabeçalhos e totais
+            table_text = re.split(r"Subtotal|Total|Resumo", table_text)[0]
+
+            # Regex: Data, Descrição e Valor (antes do "0,00" de dólar)
+            pattern = r"(\d{2}/\d{2}/\d{2})\s+(.+?)\s+(\d{1,3}(?:\.\d{3})*,\d{2})\s+0,00"
+            matches = re.findall(pattern, table_text)
+
+            for date, desc, value in matches:
+                value = float(value.replace(".", "").replace(",", "."))
+                transactions.append([date, desc.strip(), value])
+
+    # Cria DataFrame
+    df = pd.DataFrame(transactions, columns=["Data", "Descrição", "Valor (R$)"])
+    return df
+
+
+if __name__ == "__main__":
+    df = extract_transactions(FILEPATH, pw="11659")
+    print(df)
+    print(f"\n{len(df)} transações encontradas.")
