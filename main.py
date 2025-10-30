d6cfc63a55c6da0424014deb9dea1711e3ed6799
Daniela <danieladesa01@gmail.com>
2025-10-30 07:12:57 -0300

    data extraction completed

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pdfplumber
 import re
 import pandas as pd
+import os
 
-FILEPATH = "data/raw/Fatura2025-10-05.pdf"
+
+RAW_DATA = "data/raw"
+READ_FILES_PATH = "data/processed/read_files.txt"
 
 def extract_transactions(filepath, pw=None, start_page=2):
     transactions = []
@@ -19,20 +22,32 @@ def extract_transactions(filepath, pw=None, start_page=2):
 
             # print(text)
             
-            if "Data Descrição R$" in text:
-                table_text = text.split("Data Descrição R$")[-1]
-            else:
+            if "Data Descrição R$" not in text:
                 continue
-
-            table_text = re.split(r"Subtotal|Total|Resumo", table_text)[0]
-
+           
+            lines = text.split('\n')
+            table_section = False
+            table_lines = []
+            
+            for line in lines:  
+                if re.search(r"Data\s+Descrição\s+R\$", line):
+                    table_section = True
+                if re.search(r"Subtotal|Total|Resumo", line):
+                    table_section = False
+                    continue
+                if table_section:
+                    table_lines.append(line)
+                    continue
+            
             # Regex: Data, Descrição Valor
             pattern = r"(\d{2}/\d{2}/\d{2})\s+(.+?)\s+(\d{1,3}(?:\.\d{3})*,\d{2})\s+0,00"
-            matches = re.findall(pattern, table_text)
-
-            for date, desc, value in matches:
-                value = float(value.replace(".", "").replace(",", "."))
-                transactions.append([date, desc.strip(), value])
+            for line in table_lines:
+                data = re.match(pattern, line.strip())
+                if data:
+                    date, desc, value = data.groups()
+                    value = float(value.replace(".", "").replace(",", "."))
+                    transactions.append([date, desc.strip(), value])
+            
 
     # Cria DataFrame
     df = pd.DataFrame(transactions, columns=["Data", "Descrição", "Valor (R$)"])
@@ -40,6 +55,45 @@ def extract_transactions(filepath, pw=None, start_page=2):
 
 
 if __name__ == "__main__":
-    df = extract_transactions(FILEPATH, pw="11659")
-    print(df)
-    print(f"\n{len(df)} transações encontradas.")
+    
+    # Create output folder for CSV files
+    OUTPUT_DIR = "data/processed"
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    
+    # GET A STRING LIST ALL FILES OF RAW DATA 
+    all_files = [os.path.join(RAW_DATA, f) for f in os.listdir(RAW_DATA) if f.endswith('.pdf')]
+    all_dataframes = []
+
+    # READS ALL ALREADY PROCESSED FILES FORM TXT FILE
+    if os.path.exists(READ_FILES_PATH):
+        with open(READ_FILES_PATH, "r") as f:
+            read_files = [line.strip() for line in f.readlines()]
+    else:
+        read_files = []
+        
+    with open("ps.txt", "r") as f:
+        passw = f.read().strip()
+        
+    for pdf_file in all_files:
+        if pdf_file in read_files:
+            continue
+        else:
+            print(f"Processing file: {pdf_file}")
+            try:
+                df = extract_transactions(pdf_file, passw)
+                print(df)
+                
+                # Get filename without extension
+                base_name = os.path.splitext(os.path.basename(pdf_file))[0]
+                csv_filename = base_name + '.csv'
+                csv_path = os.path.join(OUTPUT_DIR, csv_filename)
+                df.to_csv(csv_path, index=False, encoding='utf-8')
+                print(f"Saved CSV file: {csv_filename}")
+                
+                all_dataframes.append(df)
+                read_files.append(pdf_file)
+                with open(READ_FILES_PATH, "a") as f:
+                    f.write(pdf_file + "\n")
+                    
+            except Exception as e:
+                print(f"Error: {pdf_file}: {str(e)}")
